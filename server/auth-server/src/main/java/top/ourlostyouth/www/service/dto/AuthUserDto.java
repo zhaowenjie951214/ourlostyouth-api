be74ca7a3b078cfdde917ed64f557b8b5046dfb4
zhaowenjie <zhaowenjie@jsjd.cc>
2022-06-16 12:54:35 +0800

    权限验证模块

--- server/auth-server/src/main/java/top/ourlostyouth/www/service/dto/AuthUserDto.java
+++ server/auth-server/src/main/java/top/ourlostyouth/www/service/dto/AuthUserDto.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package top.ourlostyouth.www.service;
+package top.ourlostyouth.www.service.dto;
 
 import lombok.Getter;
 import lombok.Setter;
