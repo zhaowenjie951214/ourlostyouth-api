41d7b4b8836533201ccbec3554019425f064f852
zhaowenjie <zhaowenjie@jsjd.cc>
2022-06-16 13:09:20 +0800

    权限验证模块

--- common/src/main/java/top/ourlostyouth/www/utils/rsa/RsaProperties.java
+++ common/src/main/java/top/ourlostyouth/www/utils/rsa/RsaProperties.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package top.ourlostyouth.www.config;
+package top.ourlostyouth.www.utils.rsa;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
